a89649ea1a0f5d68709132846e552c43f480a16d
hogashi <hogextend@gmail.com>
2019-05-25 22:23:21 +0900

    fix c

--- abc127/c.rb
+++ abc127/c.rb
@@ -7,10 +7,5 @@ m.times{|i|
   l[i], r[i] = gets.chomp.split(' ').map{|i| i.to_i }
 }
 
-oks = l + r
-
-m.times{|i|
-  oks = oks & (l.slice(i, m) + r.slice(0, i + 1))
-}
-puts oks.length
+puts r.min - l.max + 1
 
