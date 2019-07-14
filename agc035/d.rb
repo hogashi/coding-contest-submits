261f4a7f8502bb3c7684782267e8c0dc4cf80002
hogashi <azuma.ginkakuji@gmail.com>
2019-07-14 23:19:34 +0900

    WIP dp

--- agc035/d.rb
+++ agc035/d.rb
@@ -3,14 +3,21 @@ a = gets.chomp.split(' ').map{|i| i.to_i }
 
 p a
 
-(n - 2).times{|i|
-  min = 1000000001
-  index = 0
+phi = [[[]]]
+
+a[1 .. -2].each_with_index{|aa, i|
+  head = i - 1 < 0 ? [] : a[0 .. i - 1]
+  phi[0][i][0] = head + [a[i - 1] + aa, a[i + 1] + aa] + a[i + 1 .. -2]
+  sum = 0
+  phi[0][i][0].each{|ii| sum += ii }
+  phi[0][i][1] = sum
+}
+
+(n - 3).times{|ii|
+  i = ii + 2
+  phi[i] = [[]]
   a[1 .. -2].each_with_index{|aa, i|
-    if aa < min
-      min = aa
-      index = i + 1
-    end
+    phi[0][i] = a.sum + aa
   }
   head = index - 2 < 0 ? [] : a[0 .. index - 2]
   a = head + [a[index - 1] + a[index], a[index + 1] + a[index]] + a[index + 2 .. -1]
