d5f9898b8d7a5e845df0e4955ed49c62d4041f62
hogashi <azuma.ginkakuji@gmail.com>
2019-08-04 22:14:51 +0900

    second thought

--- abc136/b.rb
+++ abc136/b.rb
@@ -4,16 +4,11 @@ d = Math.log10(n).floor + 1
 
 # p d
 
-c = 0
-
-if d % 2 == 0
-  (d / 2).times{|i|
-    c += 10 ** (i * 2) * 9
-  }
-  puts c
+if d == 1
+  puts n
+elsif d % 2 == 0
+  puts '90' * (d / 2 - 1) + '9'
 else
-  ((d - 1) / 2).times{|i|
-    c += 10 ** (i * 2) * 9
-  }
-  puts c + (n - (10 ** (d - 1) - 1))
+  c =  ('90' * ((d - 1) / 2 - 1) + '9').to_i
+  puts c + (n - (('1' + ('0' * (d - 1))).to_i - 1))
 end
