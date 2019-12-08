782716a7f081bd35aeb7f7183194f4fdbd5c646a
hogashi <azuma.ginkakuji@gmail.com>
2019-12-08 22:37:09 +0900

    fix d

--- abc147/d.rb
+++ abc147/d.rb
@@ -1,14 +1,14 @@
 diver = 10 ** 9 + 7
 
 n = gets.chomp.to_i
-a = gets.chomp.split(' ').map{|i| i.to_i }
-
 sum = 0
-(n - 1).times{|i|
-  (n - (i + 1)).times{|jj|
-    j = jj + (i + 1)
-    sum += a[i] ^ a[j]
+a = []
+gets.chomp.split(' ').map{|ii|
+  i = ii.to_i
+  a.each{|j|
+    sum += i ^ j
   }
+  a.push(i)
 }
 
 if sum > diver
