a181bcfba87862b6c514e0edd5a22e478cb8d0ff
hogashi <azuma.ginkakuji@gmail.com>
2019-12-29 21:50:15 +0900

    fix e (wrong)

--- abc149/e.rb
+++ abc149/e.rb
@@ -5,12 +5,43 @@ a = gets.chomp.split(' ').map{|i| i.to_i }.sort{|a, b| b <=> a}
 
 parent = 0
 sums = []
+count = 0
 
 while parent < a.length
-  sums.push(a[parent] * 2)
+  index = sums.bsearch_index{|ii| ii > a[parent] * 2 }
+  if index.nil?
+    index = sums.length
+  elsif index > 0
+    index -= 1
+  end
+  sums.insert(index, a[parent] * 2)
+  count += 1
+  if count >= m
+    sum = 0
+    # p sums.sort{|a, b| b <=> a }[0 .. (m - 1)]
+    sums.sort{|a, b| b <=> a }[0 .. (m - 1)].each{|i| sum += i }
+
+    puts sum
+    exit
+  end
   a[(parent + 1) .. -1].each{|i|
+    index = sums.bsearch_index{|ii| ii > a[parent] * 2 }
+    if index.nil?
+      index = sums.length
+    elsif index > 0
+      index -= 1
+    end
     2.times{|t|
-      sums.push(i + a[parent])
+      sums.insert(index, i + a[parent])
+      count += 1
+      if count >= m
+        sum = 0
+        # p sums.sort{|a, b| b <=> a }[0 .. (m - 1)]
+        sums.sort{|a, b| b <=> a }[0 .. (m - 1)].each{|i| sum += i }
+
+        puts sum
+        exit
+      end
     }
   }
   parent += 1
