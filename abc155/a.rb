4bbfed30d9ec082682cb204714b635ee1c45ae87
hogashi <azuma.ginkakuji@gmail.com>
2020-02-16 21:10:43 +0900

    fix a

--- abc155/a.rb
+++ abc155/a.rb
@@ -3,14 +3,20 @@ a, b, c = gets.chomp.split(' ').map{|i| i.to_i }
 if a == b
   if a != c
     puts 'Yes'
+  else
+    puts 'No'
   end
 elsif b == c
   if a != c
     puts 'Yes'
+  else
+    puts 'No'
   end
 elsif a == c
   if a != b
     puts 'Yes'
+  else
+    puts 'No'
   end
 else
   puts 'No'
