0531d5d182ed1fd1bb822918866f66bb85c7f36d
hogashi <azuma.ginkakuji@gmail.com>
2020-02-22 23:13:43 +0900

    fix f(wrong)

--- abc156/f.rb
+++ abc156/f.rb
@@ -7,19 +7,19 @@ q.times{|i|
   dans = 0
   danss = [0]
 
-  dsummodms = [d[0] % m]
+  dsummodms = [x % m]
   (k - 1).times{|jj|
     j = jj + 1
     dsummodms[j] = (dsummodms[j - 1] + d[j]) % m
     dans += 1 if dsummodms[j - 1] < dsummodms[j]
     danss[j] = dans
   }
-  danss[0] = 1 if dsummodms[k - 1] < dsummodms[0]
+  danss[0] = 1 if dsummodms[k - 1] < ((dsummodms[0] + dsummodms[k - 1]) % m)
 
   nmodk = n % k
   sets = (n - nmodk) / k
   # p dsummodms
   # p danss
   # p [nmodk, sets]
-  puts dans * sets + (sets - (nmodk == 0 ? 1 : 0)) * danss[0]
+  puts dans * sets + (nmodk == 0 || nmodk == 1 ? 0 : danss[nmodk - 1]) + (sets - (nmodk == 0 ? 1 : 0)) * danss[0]
 }
