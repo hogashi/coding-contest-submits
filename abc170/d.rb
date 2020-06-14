51fe0c9df9e160a94c9e957ae837f7e4a2a915b4
hogashi <azuma.ginkakuji@gmail.com>
2020-06-14 22:00:54 +0900

    fix d (wrong)

--- abc170/d.rb
+++ abc170/d.rb
@@ -1,19 +1,33 @@
 n = gets.chomp.to_i
-a = gets.chomp.split(' ').map{|i| i.to_i }
+aset = {}
+gets.chomp.split(' ').each{|i|
+  ii = i.to_i
+  aset[ii] = 0 if aset[ii].nil?
+  aset[ii] += 1
+}
+as = aset.keys.sort{|a, b| b <=> a }
+
+p aset
+p as
 
 i = 0
 count = 0
-a.each{|ai|
-  j = 0
-  count += 1 unless a.any?{|aj|
-    # p [count, i, j, ai, aj, ai > aj, ai % aj === 0, ai > aj && ai % aj === 0]
-    j += 1
-    if i === (j - 1)
-      false
-    else
-      ai >= aj && ai % aj === 0
+as.each{|ai|
+  is_warikireru = false
+  if aset[ai] > 1
+    i += 1
+    next
+  end
+  (n - i - 1).times{|jj|
+    j = jj + i + 1
+    aj = as[j]
+    # p [count, i, j, ai, aj, ai % aj === 0]
+    if ai % aj === 0
+      is_warikireru = true
+      break
     end
   }
+  count += 1 unless is_warikireru
   i += 1
 }
 
