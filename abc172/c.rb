c0ddd7da2b1f3bc4219675fd0075ecd5552da439
hogashi <azuma.ginkakuji@gmail.com>
2020-06-29 14:00:24 +0900

    fix c (wrong)

--- abc172/c.rb
+++ abc172/c.rb
@@ -1,23 +1,34 @@
 n, m, k = gets.chomp.split(' ').map{|i| i.to_i }
-a = gets.chomp.split(' ').map{|i| i.to_i }
-b = gets.chomp.split(' ').map{|i| i.to_i }
+a = [0]
+b = [0]
+gets.chomp.split(' ').each{|i|
+  a.push(a.last + i.to_i)
+}
+gets.chomp.split(' ').each{|i|
+  b.push(b.last + i.to_i)
+}
+a.shift
+b.shift
 
-#     nowmin, aindex, bindex
-q = [[     0,      0,      0]]
-count = 0
+oldj = m - 1
+maxcount = 0
 
-while q.length > 0
-  oldq = q.clone
-  # p oldq
-  q = []
-
-  oldq.each{|qq|
-    nowmin, ai, bi = qq
-    q.push([nowmin + a[ai], ai + 1, bi]) if (ai < n && nowmin + a[ai] <= k)
-    q.push([nowmin + b[bi], ai, bi + 1]) if (bi < m && nowmin + b[bi] <= k)
+n.times{|i|
+  ai = a[i]
+  break if ai > k
+  (oldj + 1).times{|jj|
+    j = oldj - jj
+    count = i + j + 2
+    # next if maxcount >= count
+    bj = b[j]
+    # p [oldj, i, j, ai, bj, ai + bj, k]
+    if ai + bj <= k
+      # p [maxcount, i, j, count]
+      maxcount = count if maxcount < count
+      oldj = j
+      break
+    end
   }
+}
 
-  count += 1 if q.length > 0
-end
-
-puts count
+puts maxcount
