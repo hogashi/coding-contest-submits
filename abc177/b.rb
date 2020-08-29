56406a59b0d21220181ba6e7070cf3fc5397d7a7
hogashi <azuma.ginkakuji@gmail.com>
2020-08-29 22:13:00 +0900

    fix b

--- abc177/b.rb
+++ abc177/b.rb
@@ -3,10 +3,11 @@ t = gets.chomp
 tlength = t.length
 
 min = tlength
-(s.length - tlength).times{|i|
+(s.length - tlength + 1).times{|i|
   diff = tlength
   ss = s[i .. i + tlength - 1]
   tlength.times{|j|
+    # p [s, t, ss, i, j, ss[j], t[j]]
     diff -= 1 if ss[j] == t[j]
   }
 
