969c1d1bdd3e416a82599fb5f55d09eba41c2248
hogashi <azuma.ginkakuji@gmail.com>
2020-10-12 13:51:18 +0900

    fix b

--- arc105/b.rb
+++ arc105/b.rb
@@ -1,45 +1,28 @@
 n = gets.chomp.to_i
 as = []
 aset = {}
-a = gets.chomp.split(' ').map{|ii|
+gets.chomp.split(' ').each{|ii|
   i = ii.to_i
 
   if aset[i].nil?
     as.push(i)
     aset[i] = true
   end
-
-  i
 }
 
-as.sort!
+while as.length != 1
+  a1 = as.pop
+  a2 = as.pop
 
-# p a
-# p aset
-# p as
-# p '----- ----- -----'
-
-max = as.pop
-aset[max] = nil
-min = as[0]
-while (max != min) do
-  mm = max - min
-  index = as.bsearch_index{|v| v > mm }
-  # p as
-  # p [max, min, mm, index]
-  if index.nil?
-    max = mm
-  else
-    if aset[mm].nil?
-      as.insert(index, mm)
-      aset[mm] = true
+  while a1 != 0 && a2 != 0
+    if a1 > a2
+      a1 = a1 % a2
+    else
+      a2 = a2 % a1
     end
-    min = as[0]
-    max = as.pop
-    aset[max] = nil
   end
-  # p '----- ----- -----'
+
+  as.push(a1 + a2)
 end
 
-# p [max, min]
-puts max
+puts as[0]
