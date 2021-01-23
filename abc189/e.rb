11a74cc7fb6785c8316a8b3cf53487fa119af7ec
hogashi <azuma.ginkakuji@gmail.com>
2021-01-23 22:37:23 +0900

    add e

--- abc189/e.rb
+++ abc189/e.rb
@@ -44,18 +44,25 @@ m.times{|i|
   when '3'
     kijun = o[2].to_i
     yjikumuki = 1
-    if shogen == 2 || shogen == 3
-      yjikumuki = -1
+    if jikunamae == 1
+      yjikumuki = -1 if shogen == 2 || shogen == 3
+      genten[1] += 2 * kijun * yjikumuki
+    else
+      yjikumuki = -1 if shogen == 3 || shogen == 4
+      genten[0] += 2 * kijun * yjikumuki
     end
-    genten[1] += 2 * kijun * yjikumuki
     shogen = yhantenshogen[shogen]
   when '4'
     kijun = o[2].to_i
     xjikumuki = 1
-    if shogen == 3 || shogen == 4
-      xjikumuki = -1
+    xjikumuki = -1 if shogen == 3 || shogen == 4
+    if jikunamae == 1
+      xjikumuki = -1 if shogen == 3 || shogen == 4
+      genten[0] += 2 * kijun * xjikumuki
+    else
+      xjikumuki = -1 if shogen == 2 || shogen == 3
+      genten[1] += 2 * kijun * xjikumuki
     end
-    genten[0] += 2 * kijun * xjikumuki
     shogen = xhantenshogen[shogen]
   else
     p o
@@ -72,7 +79,24 @@ q = gets.chomp.to_i
 q.times{|i|
   a, b = gets.chomp.split(' ').map(&:to_i)
 
+  acase = all_cases[a]
+  agenten = acase[:genten]
+  ashogen = acase[:shogen]
+  ajikunamae = acase[:jikunamae]
 
+  bx = x[b]
+  by = y[b]
 
-  puts -1
+  bx, by = by, bx if ajikunamae == -1
+  case shogen
+  when 2
+    by *= -1
+  when 3
+    bx *= -1
+    by *= -1
+  when 4
+    bx *= -1
+  end
+
+  puts "#{bx} #{by}"
 }
