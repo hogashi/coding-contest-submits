5fd925cba43bb97c052e7056afec0030c9827e76
hogashi <azuma.ginkakuji@gmail.com>
2021-01-30 22:49:08 +0900

    WIP

--- abc190/d.rb
+++ abc190/d.rb
@@ -1,29 +1,28 @@
 n = gets.chomp.to_i
+nr = Math.sqrt(n).floor
+dn = 2 * n
 
-if n == 0
-  puts 1
-elsif n == 1
-  puts 2
-else
+isodd = n % 2 == 1
 
-  isodd = n % 2 == 1
+count = 0
+nr.times{|ii|
+  i = ii + 1
 
-  nr = Math.sqrt(n).floor
+  sho = 1.0 * (dn + i * (1 - i)) / (2 * i)
 
-  count = 0
-  if isodd
-    nr.times{|ii|
-      i = ii + 1
+  if sho > 0
+    p [i, sho, sho.floor, sho.floor == sho]
 
-      count += 2 if n % i == 0
-    }
+    if sho.floor == sho
+      count += 2
+    end
   else
-    nr.times{|ii|
-      i = ii + 1
+    p [i, sho, sho.ceil, sho.ceil == sho]
 
-      count += 1 if (n % i == 0) && ((i % 2 == 1) || ((n / i) % 2 == 1))
-    }
+    if sho.ceil == sho
+      count += 1
+    end
   end
+}
 
-  puts count * 2
-end
+puts count
