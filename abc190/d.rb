70750e23c5869c6f1e2baec13dd165768994ea3b
hogashi <azuma.ginkakuji@gmail.com>
2021-01-30 22:52:16 +0900

    fix d

--- abc190/d.rb
+++ abc190/d.rb
@@ -1,27 +1,17 @@
 n = gets.chomp.to_i
-nr = Math.sqrt(n).floor
 dn = 2 * n
-
-isodd = n % 2 == 1
+dnr = Math.sqrt(dn).floor
 
 count = 0
-nr.times{|ii|
+dnr.times{|ii|
   i = ii + 1
 
   sho = 1.0 * (dn + i * (1 - i)) / (2 * i)
 
-  if sho > 0
-    p [i, sho, sho.floor, sho.floor == sho]
+  # p [i, sho, sho.floor, sho.floor == sho]
 
-    if sho.floor == sho
-      count += 2
-    end
-  else
-    p [i, sho, sho.ceil, sho.ceil == sho]
-
-    if sho.ceil == sho
-      count += 1
-    end
+  if sho.floor == sho
+    count += 2
   end
 }
 
