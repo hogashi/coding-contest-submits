59fb90c986d341e8b7f61ca64fa76eb8cf01bd03
hogashi <azuma.ginkakuji@gmail.com>
2021-07-24 23:40:02 +0900

    fix c

--- abc211/c.rb
+++ abc211/c.rb
@@ -18,31 +18,27 @@ sr = gets.chomp.split('').map{|c| ctoi[c] }.filter{|i| ! i.nil? }.reverse.map{|c
   c
 }
 
-# p numid
-
 srlength = sr.length
-numidsum = [[], [], [], [], [], [], [], []]
+numidsum = [[], []]
 sum = 0
 oldid = -1
 (numid[7] + [srlength + 7]).map{|id|
-  numidsum[7].concat(Array.new(id - oldid - 1, sum) + [sum + 1])
+  numidsum[7 % 2].concat(Array.new(id - oldid - 1, sum) + [sum + 1])
   sum += 1
   oldid = id
 }
-
-# p numidsum
 
 7.times{|cc|
   c = 7 - cc - 1
   oldsum = 0
   oldid = -1
   (numid[c] + [srlength + c]).map{|id|
-    sum = (numidsum[c + 1][id] + oldsum) % waru
-    # p [c, id, numidsum[c + 1][id], oldsum, sum]
-    numidsum[c].concat(Array.new(id - oldid - 1, oldsum) + [sum])
+    sum = (numidsum[(c + 1) % 2][id] + oldsum) % waru
+    numidsum[c % 2].concat(Array.new(id - oldid - 1, oldsum) + [sum])
     oldid = id
     oldsum = sum
   }
+  numidsum[(c + 1) % 2] = []
 }
 
 puts numidsum[0][-2]
